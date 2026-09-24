4d728a208f797fa9c88d5381ea9eab47835d0e6f
Paulruiz23 <paul_106_6@hotmail.com>
2026-09-24 08:03:32 -0500

    Creando carpetas del curso

---
+++ Programación + Ampliación + ED/practicasJava/src/PrimerosPasos/Ejercicio22.java
@@ -0,0 +1,10 @@
+package PrimerosPasos;
+
+public class Ejercicio22 {
+
+    public static void main(String[] args) {
+        short a = 222;
+        System.out.println("El valor de mi variable es " + a);
+    }
+
+}
---
+++ Programación + Ampliación + ED/practicasJava/src/PrimerosPasos/Ejercicio23.java
@@ -0,0 +1,11 @@
+package PrimerosPasos;
+
+public class Ejercicio23 {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("El valor de mi variable es " + a );
+        a = 10;
+        System.out.println(a);
+        System.out.println("El valor de mi variable es " + a);
+    }
+}
